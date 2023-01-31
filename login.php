6c3f5502ceff952a831d46311a758a9785d75fcb
IshanM771 <123454910+IshanM771@users.noreply.github.com>
2023-01-31 15:38:20 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+<body>
+    <h1 style="border:black; border-width:1.3px; border-style:outset;"><center>LOGIN</center></h1><br>
+    <a href="Form.html"><center>Doctor Login</center></a>
+    <a href="read.php"><center>Patient View</center></a><br> 
+
+</body>
+ </head>
+ 
+ 
+</html>
